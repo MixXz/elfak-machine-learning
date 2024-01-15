e336b8f92d4d49e97737487da61c868cd3fe177d
MixXz <milanlukic122@gmail.com>
2024-01-15 21:50:07 +0100

    quick fix

--- project I/profiler.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from ydata_profiling import ProfileReport
-
-df = pd.read_csv('./input/raw.csv')
-
-profile = ProfileReport(df, title="Profiling Report")
-
-profile.to_file("./output/profiling_report.html")
